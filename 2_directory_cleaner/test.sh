98c6736abb8b53a8fd08aa0871eeabbc85a0d2e1
kimikoluv1996 <kimikoluv1996@proton.me>
2026-01-11 15:15:28 -0500

    dirclean v0.2

--- 2_directory_cleaner/test.sh
+++ 2_directory_cleaner/test.sh
@@ -1,5 +1,5 @@
 echo "current working directory is $(pwd)"
 rm -rf test_dir
 cp -r ../test_dir_template test_dir
-#python3 main.py --dry-run test_dir
-python3 main.py ./test_dir
+./dirclean ./test_dir
+ls ./test_dir
